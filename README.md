d7e68bd01e58034aa6087d9bb1504ed341a2f12f
Fernando Pérez <fperezmaurera@gmail.com>
2017-01-20 01:05:58 +0000

    Seguimos con tutorial de django, el modelo se actualizó para incluir algunas cosas de la BD final (comentadas) se actualiza el README para incluir como hacer migraciones y como acceder a la BD.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # anabelproject
 Anabel
 
-Para saber como crear una app para el proyecto, visitar: https://docs.djangoproject.com/en/1.10/intro/tutorial01/
+#Para saber como crear una app para el proyecto, visitar: 
+# https://docs.djangoproject.com/en/1.10/intro/tutorial01/
+
+
+# Para hacer migraciones.
+# 1- Hacer cd (moverte en las carpetas usando el terminal a enterate): cd ~/workspace/enterate
+#    Esta es la carpeta del primer entérate.
+# 2- Preparar la migración: python3 manage.py makemigrations
+# 3- Realizar la migración: python3 manage.py migrate
+
+# Para ingresar a la base de datos.
+# Para iniciar sesión en la BD de polls: psql -h localhost -U fernandobperezm -d polls
+# Para ver el contenido de una tabla: SELECT * FROM <NombreDeTabla>
--- enterate/polls/models.py
+++ enterate/polls/models.py
@@ -1,12 +1,43 @@
 from django.db import models
 
+#MODELO DE LA BASE DE DATOS. 
+#conjunto de tablas, junto con sus atributos y propiedades. 
+#las clases son las tablas
+#los atributos son las columnas
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+
+
+# https://docs.djangoproject.com/en/1.10/ref/models/fields/#django.db.models 
+# Referencia de todos los tipos.
+#migracion: actualizas las bases de datos, la forma o atributos, hasta nombres
+
+
+
+class Question(models.Model):     #crea tabla
+    question_text = models.CharField(max_length=200) #crea las columnas
     pub_date = models.DateTimeField('date published')
-
+    def __str__(self):      #para impresion bonita cuando hago Question.objects.all() en el 
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+# class Usuarios(models.Model):     #crea tabla
+#     Nombre = models.CharField(max_length=20) #crea las columnas
+#     Apellido = models.CharField(max_length=25)
+#     Cedula = models.CharField(max_length=20, primary_key = True) #es char porque no voy a hacer operaciones con ese valor
+#     Email = models.EmailField()
+    
+# class Gustos(models.Model):
+#     Mequiere = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
+#     #el primer argumento llama a la esa tabla porque no puede existir respuest sin pregunta
+#     #el on_delete.cascade cuando se borre a lo que él hacía referencia, eso se borra.
+#     Cuanto = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+
+
