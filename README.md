4c78e080f2fd7b877678c311f412b15d1e29eeaa
Fernando Pérez <fperezmaurera@gmail.com>
2017-01-21 01:19:14 +0000

    ?

--- README.md
+++ README.md
@@ -4,13 +4,27 @@ Anabel
 #Para saber como crear una app para el proyecto, visitar: 
 # https://docs.djangoproject.com/en/1.10/intro/tutorial01/
 
+# Para activar postgres en C9: https://community.c9.io/t/setting-up-postgresql/1573
+# escribir en el terminal: sudo service postgresql start             es el manejador de base de datos
 
+#activar 
 # Para hacer migraciones.
 # 1- Hacer cd (moverte en las carpetas usando el terminal a enterate): cd ~/workspace/enterate
 #    Esta es la carpeta del primer entérate.
 # 2- Preparar la migración: python3 manage.py makemigrations
 # 3- Realizar la migración: python3 manage.py migrate
 
+
+#para ver las tablitas
 # Para ingresar a la base de datos.
 # Para iniciar sesión en la BD de polls: psql -h localhost -U fernandobperezm -d polls
-# Para ver el contenido de una tabla: SELECT * FROM <NombreDeTabla>
+# Para ver el contenido de una tabla: SELECT * FROM <NombreDeTabla>;
+#para salirme del terminal ctrl d 
+#\d ver tablas
+#ctrl l limpiar terminal
+
+
+# git status  todos los archivos que han cambiado
+
+
+
--- enterate/polls/models.py
+++ enterate/polls/models.py
@@ -16,6 +16,7 @@ from django.db import models
 class Question(models.Model):     #crea tabla
     question_text = models.CharField(max_length=200) #crea las columnas
     pub_date = models.DateTimeField('date published')
+    
     def __str__(self):      #para impresion bonita cuando hago Question.objects.all() en el 
         return self.question_text
 
@@ -23,21 +24,74 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.choice_text
 
-# class Usuarios(models.Model):     #crea tabla
-#     Nombre = models.CharField(max_length=20) #crea las columnas
-#     Apellido = models.CharField(max_length=25)
-#     Cedula = models.CharField(max_length=20, primary_key = True) #es char porque no voy a hacer operaciones con ese valor
-#     Email = models.EmailField()
+#usuarios(cédula, nombre, apellido, teléfono, email, contraseña)
+class Usuarios(models.Model):     #crea tabla
+    nombre = models.CharField(max_length=20) #crea las columnas
+    apellido = models.CharField(max_length=25)
+    cedula = models.CharField(max_length=20, primary_key = True) #es char porque no voy a hacer operaciones con ese valor
+    telefono = models.CharField(max_length=25)
+    email = models.EmailField()
+    contrasena = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.choice_text
+        
+#Estacionamiento(RIF, nombre, Numero_de_puestos)        
+class Estacionamiento(models.Model):
+    rIF = models.CharField(max_length=20, primary_key = True)
+    nombre = models.CharField(max_length=200)
+    numero_de_puestos = models.IntegerField(default=1000)
+    
+    def __str__(self):
+        return self.choice_text
+        
+#vehiculos(placa,CEDULA,)   
+class Vehiculos(models.Model):
+    cedula = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
+    placa =models.CharField(max_length=20, primary_key = True)
+    
+    def __str__(self):
+        return self.choice_text
+
+# alertas(numero,tipo)
+class Alertas(models.Model):
+    numero_alertas = models.AutoField(primary_key = True) 
+    tipo = models.CharField(max_length=200)
+    
+#Ocurre_a(Cedula,numero,fecha)
+class Ocurre_a(models.Model):
+    cedula_usuarios_en_alertas = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
+    numero_alertas = models.ForeignKey(Alertas, on_delete=models.CASCADE )
+    fecha_alertas    = models.DateTimeField('fecha de alerta')
+    
+    def __str__(self):
+        return self.choice_text
+        
+#Ocurre_en(RIF,numero,fecha)
+class Ocurre_en(models.Model):
+    rif = models.ForeignKey( Estacionamiento, on_delete=models.CASCADE) 
+    numero_alertas = models.ForeignKey( Alertas, on_delete=models.CASCADE)
+    fecha_alertas    = models.DateTimeField('fecha de alerta')
+    
+    def __str__(self):
+        return self.choice_text
+
+#Ticket( placa, RIF,numero,hora_entrada, hora salida)
+class Ticket(models.Model):
+    placa = models.ForeignKey(Vehiculos, on_delete=models.CASCADE) 
+    RIF = models.ForeignKey(Estacionamiento, on_delete=models.CASCADE)
+    Numero_ticket = models.IntegerField(default=0)
+    Hora_entrada =  models.DateTimeField('Hora de entrada')
+    Hora_salida =  models.DateTimeField('Hora de salida')
+    def __str__(self):
+        return self.choice_text
+
+# # GUSTOS(cédula, gusto_entrada, gusto_sal)
 # class Gustos(models.Model):
-#     Mequiere = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
-#     #el primer argumento llama a la esa tabla porque no puede existir respuest sin pregunta
-#     #el on_delete.cascade cuando se borre a lo que él hacía referencia, eso se borra.
-#     Cuanto = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
-
-
-
+#     cedula = models.ForeignKey(Usuarios, on_delete=models.CASCADE) 
+#     gusto_entrada = models.CharField(max_length=200)
+#     gusto_salida = models.CharField(max_length=200)
