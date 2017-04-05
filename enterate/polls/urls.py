1285f2ef6facb80d726ee9db06212ffcbc277153
Fernando Pérez <fperezmaurera@gmail.com>
2017-04-05 18:06:05 +0000

    Added the urls to get a question, it's result and to vote for it. Also, added the view which response adapts to each of those cases.

--- enterate/polls/urls.py
+++ enterate/polls/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<cedula_usuarios>(V\-[0-9]+))/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     
+    # ex: /polls/V-1234567
+    url(r'^(?P<cedula_usuarios>(V\-[0-9]+))/$', views.cedula, name='cedula'),
+    # ex: /polls/<Cualquier palabra sin a y que no esté antes>
     url(r'^(?P<saludos_fer>([b-z]+))/$', views.saludos, name='saludos para ti'),
  ]
