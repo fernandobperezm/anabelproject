872ccf7cc824ab0f124958b00f39fda3120d57ed
Fernando Pérez <fperezmaurera@gmail.com>
2017-04-05 16:52:44 +0000

    Finished django tutorial, part 2.

--- enterate/polls/admin.py
+++ enterate/polls/admin.py
@@ -1,3 +1,13 @@
+#añadir cosas a la pagina web desde la base de datos. 
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuarios #lo importa al archivo admin.py
+from .models import Estacionamiento
+from .models import Vehiculos
+
+#para ponerlo en la BD
+admin.site.register(Usuarios)
+admin.site.register(Estacionamiento)
+admin.site.register(Vehiculos)
--- enterate/polls/views.py
+++ enterate/polls/views.py
@@ -1,5 +1,12 @@
+#Define las vistas que existen
+
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index2.")
+    
+def detail(request, cedula_usuarios):
+    return HttpResponse("You're looking at user %s." % cedula_usuarios)    
+
+def saludos(request, saludos_fer): #cada página que tendré
+    return HttpResponse("Vamos a mandarte saludos sin la a :  %s." % saludos_fer)
